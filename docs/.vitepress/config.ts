175d2599b3fb5d9ea701fef49dc950d0da553b30
FrankBevr <frank_dierolf@web.de>
2023-10-21 12:13:42 +0200

    :abc: (./docs) adjust vitepress, include files

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -22,10 +22,23 @@ export default {
         text: "The Future",
         items: [{ text: "Markdown Examples", link: "/markdown-examples" }],
       },
+      {
+        text: "Notes",
+        items: [
+          { text: "Launch Event", link: "/Notes-LaunchEvent" },
+          { text: "Get Block", link: "/Archy_GetBlock" },
+          { text: "Software Design", link: "/Software-Design" },
+          { text: "AZero", link: "/dennis_integrate_azero_id" },
+          { text: "Ink Championship", link: "/german_ink_championship" },
+          { text: "Safe and Robust Ink", link: "/piotr_safeAndRobuts" },
+          { text: "Ink Storage", link: "/uladzislau_deep_dive_ink_storage" },
+          { text: "Org Recap", link: "/orgRecap/recap" },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+      { icon: "github", link: "https://github.com/frankbevr/inkora" },
     ],
   },
 };
