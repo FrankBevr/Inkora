1b029a2f01345ec2d7dc86ca95f2511dd2abc78c
FrankBevr <frank_dierolf@web.de>
2023-10-22 23:20:02 +0200

    :zap: (./docs) update vitepress config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,38 +1,43 @@
-// https://vitepress.dev/reference/site-config
 export default {
-  title: "✒️ Inkora",
+  title: "Inkora",
   description: "Moe's Taverne",
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     nav: [{ text: "Home", link: "/" }],
-
+    logo: "https://use.ink/assets/files/ink-squink-129208fde8d6d4a8474bf71865e3d76b.svg",
     sidebar: [
       {
         text: "The Beginning",
         items: [
-          { text: "Markdown Examples", link: "/markdown-examples" },
-          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Markdown Examples", link: "/Notes/markdown-examples" },
         ],
       },
       {
-        text: "The Present",
-        items: [{ text: "Markdown Examples", link: "/markdown-examples" }],
+        text: "The Road",
+        items: [
+          { text: "Saturday", link: "/Journey/Saturday" },
+          { text: "Sunday", link: "/Journey/Sunday" },
+        ],
       },
       {
         text: "The Future",
-        items: [{ text: "Markdown Examples", link: "/markdown-examples" }],
+        items: [
+          { text: "Markdown Examples", link: "/Notes/markdown-examples" },
+        ],
       },
       {
         text: "Notes",
         items: [
-          { text: "Launch Event", link: "/Notes-LaunchEvent" },
-          { text: "Get Block", link: "/Archy_GetBlock" },
-          { text: "Software Design", link: "/Software-Design" },
-          { text: "AZero", link: "/dennis_integrate_azero_id" },
-          { text: "Ink Championship", link: "/german_ink_championship" },
-          { text: "Safe and Robust Ink", link: "/piotr_safeAndRobuts" },
-          { text: "Ink Storage", link: "/uladzislau_deep_dive_ink_storage" },
-          { text: "Org Recap", link: "/orgRecap/recap" },
+          { text: "Launch Event", link: "/Notes/Notes-LaunchEvent" },
+          { text: "Get Block", link: "/Notes/Archy_GetBlock" },
+          { text: "Software Design", link: "/Notes/Software-Design" },
+          { text: "AZero", link: "/Notes/dennis_integrate_azero_id" },
+          { text: "Ink Championship", link: "/Notes/german_ink_championship" },
+          { text: "Safe and Robust Ink", link: "/Notes/piotr_safeAndRobuts" },
+          {
+            text: "Ink Storage",
+            link: "/Notes/uladzislau_deep_dive_ink_storage",
+          },
+          { text: "Org Recap", link: "/Notes/org_recap" },
         ],
       },
     ],
