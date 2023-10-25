aec298ce48c04c461e27b5ee1e899c08e534367b
FrankBevr <frank_dierolf@web.de>
2023-10-25 12:15:54 +0200

    :zap: (./frontend) convert to vite

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './App.css';
-import MindARViewer from './mindar-viewer';
 import MindARThreeViewer from './mindar-three-viewer';
 
 function App() {
@@ -11,17 +10,9 @@ function App() {
       <h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank">MindAR</a></h1>
 
       <div className="control-buttons">
-        {started === null && <button onClick={() => {setStarted('aframe')}}>Start AFRAME version</button>}
         {started === null && <button onClick={() => {setStarted('three')}}>Start ThreeJS version</button>}
         {started !== null && <button onClick={() => {setStarted(null)}}>Stop</button>}
       </div>
-
-      {started === 'aframe' && (
-        <div className="container">
-          <MindARViewer/>
-          <video></video>
-        </div>
-      )}
 
       {started === 'three' && (
         <div className="container">
