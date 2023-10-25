32243c977245030fa6a0a6bed72c2e0f210b5d98
FrankBevr <frank_dierolf@web.de>
2023-10-25 11:50:37 +0200

    :truck: (./frontend) reshuffle project, add playground folder

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,3 +1,11 @@
-import mind from "./mind.js";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-mind();
+//import {Controller} from 'mind-ar/dist/mindar-image.prod.js';
+//console.log("MindARThree", Controller);
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab="home" />);
