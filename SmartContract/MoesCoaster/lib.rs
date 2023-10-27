22159197ede2d28d4373b059b73c9eb83c5fb3e7
FrankBevr <frank_dierolf@web.de>
2023-10-27 12:23:47 +0200

    :zap: (./SmartContract) add Journey function for scratch Card Functionality

--- SmartContract/MoesCoaster/lib.rs
+++ SmartContract/MoesCoaster/lib.rs
@@ -2,23 +2,52 @@
 
 #[ink::contract]
 mod moes_coaster {
-
     use ink::prelude::string::String;
 
     #[ink(storage)]
     pub struct MoesCoaster {
-        value: String,
+        ipfs_link: String,
+        money: u128,
     }
 
     impl MoesCoaster {
+        // Instaniate with stupid values
         #[ink(constructor)]
         pub fn new() -> Self {
-            Self { value: "ipfs://abc".into() }
+            Self {
+                ipfs_link: "ipfs://abc".into(),
+                money: 0,
+            }
         }
 
+        // Get 3D Model
         #[ink(message)]
-        pub fn get(&self) -> String {
-            self.value.clone()
+        pub fn get_ipfs_link(&self) -> String {
+            self.ipfs_link.clone()
+        }
+
+        //  main function
+        #[ink(message)]
+        pub fn participate_scratch_card(&self, random_number: u8) -> u8 {
+            random_number
+        }
+
+        // Journey function to transfer money to contract
+        #[ink(message)]
+        pub fn feed_me(&self) -> bool {
+            true
+        }
+
+        // Journey function to transfer money from contract to caller
+        #[ink(message)]
+        pub fn puke_it(&self) -> bool {
+            true
+        }
+
+        // Journey function create ranomness 
+        #[ink(message)]
+        pub fn generate_random_number(&self) -> u8 {
+            1
         }
     }
 
@@ -28,16 +57,8 @@ mod moes_coaster {
 
         #[ink::test]
         fn default_works() {
-            let MoesCoaster = MoesCoaster::new();
-            assert_eq!(MoesCoaster.get(), "ipfs://abc");
+            let moes_coaster = MoesCoaster::new();
+            assert_eq!(moes_coaster.get_ipfs_link(), "ipfs://abc");
         }
-
-        // #[ink::test]
-        // fn it_works() {
-        //     let mut MoesCoaster = MoesCoaster::new(false);
-        //     assert_eq!(MoesCoaster.get(), false);
-        //     MoesCoaster.flip();
-        //     assert_eq!(MoesCoaster.get(), true);
-        // }
     }
 }
