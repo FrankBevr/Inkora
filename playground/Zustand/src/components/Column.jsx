803044c2108d621e4478d8914ae35cd05baf896c
FrankBevr <frank_dierolf@web.de>
2023-10-31 11:44:11 +0100

    :zap: (./playground) add add Tasks

--- playground/Zustand/src/components/Column.jsx
+++ playground/Zustand/src/components/Column.jsx
@@ -1,18 +1,39 @@
+import { useState } from 'react'
 import { useTaskStore } from '../store'
 import './Column.css'
 import Task from './Task'
 import { shallow } from 'zustand/shallow'
 
 const Column = ({ state }) => {
+  const [text, setText] = useState('')
+  const [open, setOpen] = useState(false)
   const tasks = useTaskStore(
     (store) => store.tasks.filter((task) => task.state === state),
     shallow
   );
+  const addTask = useTaskStore(store => store.addTask);
   return <div className="column" >
-    <p>{state}</p>
-    {tasks.map((task) => (
-      <Task title={task.title} key={task.title} />
-    ))}
+    <div className='titleWrapper'>
+      <p>{state}</p>
+      <button onClick={() => setOpen(true)}>Add</button>
+    </div>
+    <div>
+      {tasks.map((task) => (
+        <Task title={task.title} key={task.title} />
+      ))}
+    </div>
+    {open && (
+      <div className='Modal'>
+        <div className='modalContent'>
+          <input onChange={(e) => setText(e.target.value)} value={text}></input>
+          <button onClick={() => {
+            addTask(text, state);
+            setText('')
+            setOpen(false)
+          }}>Submit</button>
+        </div>
+      </div>
+    )}
   </div>
 }
 
--- playground/Zustand/src/store.js
+++ playground/Zustand/src/store.js
@@ -2,5 +2,7 @@ import { create } from 'zustand'
 
 export const useTaskStore = create((set) => ({
   tasks: [{ title: "TestTask", state: "ONGOING" }],
+  addTask: (title, state) => set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+  deleteTask: (title) => set((store) => ({ tasks: store.tasks.filter(task => task.title === title) }))
 }))
 
