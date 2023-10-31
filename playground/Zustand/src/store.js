a59368b2b741de4da8d73de01f7d802e880a7278
FrankBevr <frank_dierolf@web.de>
2023-10-31 15:45:15 +0100

    :zap: (./playground) add devtools, add persitence, add cstm midlwa

--- playground/Zustand/src/store.js
+++ playground/Zustand/src/store.js
@@ -1,19 +1,51 @@
 import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
-export const useTaskStore = create((set) => ({
-  tasks: [{ title: "TestTask", state: "ONGOING" }],
-  draggedTask: null,
-  addTask: (title, state) =>
-    set((store) => ({ tasks: [...store.tasks, { title, state }] })),
-  deleteTask: (title) =>
-    set((store) => ({
-      tasks: store.tasks.filter((task) => task.title !== title),
-    })),
-  setDraggedTask: (title) => set({ draggedTask: title }),
-  moveTask: (title, state) =>
-    set((store) => ({
-      tasks: store.tasks.map((task) =>
-        task.title === title ? { title, state } : task,
-      ),
-    })),
-}));
+const log = (config) => (set, get, api) =>
+  config(
+    (...args) => {
+      console.log(args);
+      set(...args);
+    },
+    get,
+    api
+  );
+
+export const useTaskStore = create(
+  log(
+    persist(
+      devtools((set) => ({
+        tasks: [],
+        draggedTask: null,
+        addTask: (title, state) =>
+          set(
+            (store) => ({ tasks: [...store.tasks, { title, state }] }),
+            false,
+            "addTask",
+          ),
+        deleteTask: (title) =>
+          set(
+            (store) => ({
+              tasks: store.tasks.filter((task) => task.title !== title),
+            }),
+            false,
+            "deleteTask",
+          ),
+        setDraggedTask: (title) =>
+          set({ draggedTask: title }, false, "setDraggedTask"),
+        moveTask: (title, state) =>
+          set(
+            (store) => ({
+              tasks: store.tasks.map((task) =>
+                task.title === title ? { title, state } : task,
+              ),
+            }),
+            false,
+            "moveTask",
+          ),
+      })),
+      { name: "TaskStore" },
+    ),
+  ),
+);
+
