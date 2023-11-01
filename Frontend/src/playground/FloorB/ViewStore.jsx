ba7ea6ac366eed35391caa2b50d686e4fa00ec17
FrankBevr <frank_dierolf@web.de>
2023-11-01 11:12:53 +0100

    :zap: (./Frontend) update Data to Store

--- Frontend/src/playground/FloorB/ViewStore.jsx
+++ Frontend/src/playground/FloorB/ViewStore.jsx
@@ -4,13 +4,23 @@ import { useState } from "react";
 const ViewStore = () => {
   const data = useExampleStore((store) => store.data);
   const addData = useExampleStore((store) => store.addData);
+  const updateData = useExampleStore((store) => store.updateData);
 
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
   const [food, setFood] = useState("");
+  const [id, setId] = useState("");
 
   const handleSubmit = () => {
     addData(name, Number(age), food);
+    setName("");
+    setAge("");
+    setFood("");
+  };
+
+  const handleUpdate = () => {
+    updateData(Number(id), name, Number(age), food);
+    setId("");
     setName("");
     setAge("");
     setFood("");
@@ -20,6 +30,7 @@ const ViewStore = () => {
       <h1>ViewStore</h1>
       {data.map((item, index) => (
         <div key={index}>
+          <p>Thats my id: {item.id}</p>
           <p>Thats my name: {item.name}</p>
           <p>Thats my age: {item.age}</p>
           <p>Thats my favorite food: {item.food}</p>
@@ -58,6 +69,32 @@ const ViewStore = () => {
       />
       <button onClick={handleSubmit}>Submit</button>
       <h2>Update Data to Store</h2>
+      <h2>Update Data to Store</h2>
+      <input
+        type="number"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+        placeholder="ID"
+      />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name"
+      />
+      <input
+        type="number"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+        placeholder="Age"
+      />
+      <input
+        type="text"
+        value={food}
+        onChange={(e) => setFood(e.target.value)}
+        placeholder="Favorite Food"
+      />
+      <button onClick={handleUpdate}>Update</button>
       <h2>Delete Data to Store</h2>
     </>
   );
--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,10 +1,19 @@
 import { create } from "zustand";
 
 export const useExampleStore = create((set) => ({
-  data: [{ name: "Frank", age: 30, food: "everything" }],
+  data: [{ id: 0, name: "Frank", age: 30, food: "everything" }],
   addData: (name, age, food) =>
-    set(
-      (store) => ({ data: [...store.data, { name, age, food }] }),
-    ),
-}))
+    set((store) => {
+      const newId = store.data.length;
+      const newData = { id: newId, name, age, food };
+      return { data: [...store.data, newData] };
+    }),
+  updateData: (id, name, age, food) =>
+    set((store) => {
+      const newData = store.data.map((item) =>
+        item.id === id ? { id, name, age, food } : item
+      );
+      return { data: newData };
+    }),
+}));
 
