080f10bc1f5928b57ec35e2bc0d10a8239d86c3e
FrankBevr <frank_dierolf@web.de>
2023-11-02 11:04:06 +0100

    :zap: (./Frontend) replace cube with mushroom

--- Frontend/src/components/mindar-three-viewer.jsx
+++ Frontend/src/components/mindar-three-viewer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
 import * as THREE from "three";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
 export default () => {
   const containerRef = useRef(null);
@@ -16,24 +17,35 @@ export default () => {
     });
     const { renderer, scene, camera } = mindarThree;
     const anchor = mindarThree.addAnchor(0);
-    const geometry = new THREE.PlaneGeometry(1, 0.55);
-    const material = new THREE.MeshBasicMaterial({
-      color: 0x00ffff,
-      transparent: true,
-      opacity: 0.5,
-    });
-    const plane = new THREE.Mesh(geometry, material);
 
-    const geometry2 = new THREE.BoxGeometry(1, 1, 1);
-    const material2 = new THREE.MeshBasicMaterial({
-      color: 0x0000ff,
-      transparent: true,
-      opacity: 0.5,
-    });
-    const cube = new THREE.Mesh(geometry2, material2);
+    const textureLoader = new THREE.TextureLoader();
+    const objLoader = new OBJLoader();
 
-    anchor.group.add(plane);
-    anchor.group.add(cube);
+    textureLoader.load(
+      "/mushroom_texture.jpg",
+      (texture) => {
+        const newMaterial = new THREE.MeshBasicMaterial({ map: texture, opacity: 0.90, transparent: true });
+
+        objLoader.load(
+          "/mushroom.obj",
+          (object) => {
+            object.traverse((node) => {
+              if (node.isMesh) {
+                node.material = newMaterial;
+              }
+            });
+            object.scale.set(0.1, 0.1, 0.1);
+            anchor.group.add(object);
+          },
+          (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+          },
+          (error) => console.error(error)
+        );
+      },
+      undefined,
+      (error) => console.error(error)
+    );
 
     mindarThree.start();
     renderer.setAnimationLoop(() => {
